b0e10052719d6c9ed4b840b4077643feee4f5631
Tomasz Kurylik <fulcrumone@icloud.com>
2023-09-22 15:50:41 +0200

    1

--- Sources/MTimer.swift
+++ Sources/MTimer.swift
@@ -9,15 +9,16 @@
 //  Copyright ©2023 Mijick. Licensed under MIT License.
 
 
-import Foundation
+import SwiftUI
 
 public class MTimer {
     private var internalTimer: Timer!
 
-    private var status: TimerStatus = .paused
-    private var startDate: Date? = nil
+    private var running: Bool = true
+    private var backgroundDate: Date? = nil
     private var runningTime: TimeInterval = 0
     private var timeInterval: TimeInterval = 0
+    private var completion: (TimeInterval) -> () = { _ in }
 
     private static let shared: MTimer = .init()
 }
@@ -25,7 +26,11 @@ public class MTimer {
 // MARK: - Timer Controls
 extension MTimer {
     public static func start(timeInterval: TimeInterval, _ completion: @escaping (TimeInterval) -> ()) { DispatchQueue.main.async {
-        shared.startDate = .init()
+        appStateBinding()
+
+        shared.running = true
+        shared.completion = completion
+
         shared.timeInterval = timeInterval
         shared.internalTimer = .scheduledTimer(withTimeInterval: timeInterval, repeats: true, block: { a in
             shared.runningTime += timeInterval
@@ -35,10 +40,18 @@ extension MTimer {
     }}
     public static func stop() {
         shared.internalTimer.invalidate()
-        shared.startDate = nil
+        shared.running = false
+
+
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didEnterBackgroundNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.willEnterForegroundNotification, object: nil)
     }
     public static func pause() {
-        
+        shared.internalTimer.invalidate()
+        shared.running = false
+
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didEnterBackgroundNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.willEnterForegroundNotification, object: nil)
     }
     public static func reset() {
 
@@ -50,11 +63,33 @@ extension MTimer {
 }
 
 extension MTimer {
+    @objc fileprivate static func didEnterBackgroundNotification() {
+        shared.internalTimer.invalidate()
+        shared.backgroundDate = .init()
+    }
 
+    @objc fileprivate static func willEnterForegroundNotification() {
+        if shared.running {
+            shared.runningTime += Date().timeIntervalSince(shared.backgroundDate!)
+        }
+        shared.completion(shared.runningTime)
+
+        shared.internalTimer = .scheduledTimer(withTimeInterval: shared.timeInterval, repeats: true, block: { a in
+            shared.runningTime += shared.timeInterval
+            shared.completion(shared.runningTime)
+        })
+
+
+        shared.backgroundDate = nil
+    }
 }
 
 extension MTimer {
+    fileprivate static func appStateBinding() {
 
+        NotificationCenter.default.addObserver(self, selector: #selector(didEnterBackgroundNotification), name: UIApplication.didEnterBackgroundNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(willEnterForegroundNotification), name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
 }
 
 
