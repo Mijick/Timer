e730b19cf415bffbb2a56fe5c36fedab8e251cc5
Tomasz Kurylik <fulcrumone@icloud.com>
2023-09-25 00:12:19 +0200

    1

--- Tests/TimerTests.swift
+++ Tests/TimerTests.swift
@@ -139,19 +139,15 @@ extension TimerTests {
 }
 
 
-
-
-extension XCTestCase {
-
+// MARK: - Helpers
+private extension XCTestCase {
     func wait(for duration: TimeInterval) {
         let waitExpectation = expectation(description: "Waiting")
 
-        let when = DispatchTime.now() + duration
-        DispatchQueue.main.asyncAfter(deadline: when) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
             waitExpectation.fulfill()
         }
 
-        // We use a buffer here to avoid flakiness with Timer on CI
         waitForExpectations(timeout: duration + 0.5)
     }
 }
