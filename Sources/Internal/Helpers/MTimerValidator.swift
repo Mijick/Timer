7b0d42635c1cdda45c5f09355d08803d5f940641
jay-jay-lama <alina.petrovskaia92@gmail.com>
2024-11-23 14:40:09 +0100

    1

--- Sources/Internal/Helpers/MTimerValidator.swift
+++ Sources/Internal/Helpers/MTimerValidator.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 class MTimerValidator {
-    func checkRequirementsForInitializingTimer(_ publisherTime: TimeInterval) throws {
+    static func checkRequirementsForInitializingTimer(_ publisherTime: TimeInterval) throws {
         if publisherTime < 0.001 { throw MTimerError.publisherTimeCannotBeLessThanOneMillisecond }
     }
-    func checkRequirementsForStartingTimer(_ startTime: TimeInterval, _ endTime: TimeInterval, _ state: MTimerStateManager, _ status: MTimerStatus) throws {
+    static func checkRequirementsForStartingTimer(_ startTime: TimeInterval, _ endTime: TimeInterval, _ state: MTimerStateManager, _ status: MTimerStatus) throws {
         if startTime < 0 || endTime < 0 { throw MTimerError.timeCannotBeLessThanZero }
         if startTime == endTime { throw MTimerError.startTimeCannotBeTheSameAsEndTime }
         if status == .inProgress && state.backgroundTransitionDate == nil { throw MTimerError.timerIsAlreadyRunning }
     }
-    func checkRequirementsForResumingTimer(_ callbacks: MTimerCallbacks) throws {
+    static func checkRequirementsForResumingTimer(_ callbacks: MTimerCallbacks) throws {
         if callbacks.onRunningTimeChange == nil { throw MTimerError.cannotResumeNotInitialisedTimer }
     }
 }
